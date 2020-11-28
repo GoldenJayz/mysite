b928ca65173ea3547bb610202fd889d25c58092f
Jaden D <45317852+GoldenJayz@users.noreply.github.com>
2020-11-27 20:40:03 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,16 +2,19 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import *
+import os
+import json
 
 app = Flask(__name__)
 
+
 @app.route("/home")
 def home():
-  return render_template("index.html")
+  return render_template("puckdata.html")
 
 @app.route("/")
 def homee():
-  return render_template("index.html")
+  return render_template("puckdata.html")
 
 @app.route("/github")
 def github():
@@ -24,6 +27,22 @@ def puck():
 @app.route("/about")
 def about():
   return render_template("about.html")
+
+@app.route("/html")
+def html():
+  return render_template("puckdata.html")
+
+@app.route("/home/puck")
+def phuck():
+  return redirect("https://github.com/GoldenJayz/puck")
+
+@app.route("/home/logs")
+def logs():
+  return render_template("logs.html")
+
+@app.route("/home/music")
+def music():
+  return redirect("https://github.com/GoldenJayz/puck2")
 
 
 if __name__ == "__main__":
